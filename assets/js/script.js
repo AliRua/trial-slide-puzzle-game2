a72d7c30f609192600b42db3b424dd9353e4272b
AliRua <>
2023-07-24 14:29:58 +0000

    Add all new style and js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function shufflePuzzle() {
+    const tiles = document.querySelectorAll('.puzzle-tile');
+    const tileArray = Array.from(tiles);
+
+    for (let i = tileArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tileArray[i].innerText, tileArray[j].innerText] = [tileArray[j].innerText, tileArray[i].innerText];
+    }
+}
+
+// Call the shuffle function when the page loads
+window.onload = shufflePuzzle;
+
+function onTileClick() {
+    const emptyTile = document.querySelector('.puzzle-tile:empty');
+    const currentTile = this;
+
+    const emptyIndex = [...emptyTile.parentElement.children].indexOf(emptyTile);
+    const currentIndex = [...currentTile.parentElement.children].indexOf(currentTile);
+
+    // Check if the clicked tile is adjacent to the empty space
+    if (Math.abs(emptyIndex - currentIndex) === 1 || Math.abs(emptyIndex - currentIndex) === 3) {
+        [emptyTile.innerText, currentTile.innerText] = [currentTile.innerText, emptyTile.innerText];
+    }
+}
+
+// Add click event listener to each tile
+const tiles = document.querySelectorAll('.puzzle-tile');
+tiles.forEach(tile => tile.addEventListener('click', onTileClick));
