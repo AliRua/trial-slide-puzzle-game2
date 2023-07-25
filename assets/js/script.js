89da14184186778e8e31c4f7dba4add77e9984f2
AliRua <>
2023-07-25 10:33:54 +0000

    Update js code for incrementor

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,29 +8,30 @@ function shufflePuzzle() {
     }
 }
 
-// Add increment score function
+let moves = 0; // Initialize moves to 0
+
 function incrementScore() {
     moves++;
     document.getElementById('score').textContent = 'Turns: ' + moves;
+}
 
+// Call the shuffle function when the page loads
+window.onload = shufflePuzzle;
 
-    // Call the shuffle function when the page loads
-    window.onload = shufflePuzzle;
+function onTileClick() {
+    const emptyTile = document.querySelector('.puzzle-tile:empty');
+    const currentTile = this;
 
-    function onTileClick() {
-        const emptyTile = document.querySelector('.puzzle-tile:empty');
-        const currentTile = this;
+    const emptyIndex = [...emptyTile.parentElement.children].indexOf(emptyTile);
+    const currentIndex = [...currentTile.parentElement.children].indexOf(currentTile);
 
-        const emptyIndex = [...emptyTile.parentElement.children].indexOf(emptyTile);
-        const currentIndex = [...currentTile.parentElement.children].indexOf(currentTile);
-
-        // Check if the clicked tile is adjacent to the empty space
-        if (Math.abs(emptyIndex - currentIndex) === 1 || Math.abs(emptyIndex - currentIndex) === 3) {
-            [emptyTile.innerText, currentTile.innerText] = [currentTile.innerText, emptyTile.innerText];
-            incrementScore();
-        }
+    // Check if the clicked tile is adjacent to the empty space
+    if (Math.abs(emptyIndex - currentIndex) === 1 || Math.abs(emptyIndex - currentIndex) === 3) {
+        [emptyTile.innerText, currentTile.innerText] = [currentTile.innerText, emptyTile.innerText];
+        incrementScore();
     }
+}
 
-    // Add click event listener to each tile
-    const tiles = document.querySelectorAll('.puzzle-tile');
-    tiles.forEach(tile => tile.addEventListener('click', onTileClick));
+// Add click event listener to each tile
+const tiles = document.querySelectorAll('.puzzle-tile');
+tiles.forEach(tile => tile.addEventListener('click', onTileClick));
